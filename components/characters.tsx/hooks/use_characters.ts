9097ca46fae9494cb85346dce65099144db97b02
benabarre90 <ebenabarre@getfivvy.com>
2024-05-15 19:31:52 -0300

    add some testing

--- components/characters.tsx/hooks/use_characters.ts
+++ components/characters.tsx/hooks/use_characters.ts
@@ -4,6 +4,24 @@ import { CharacterModel } from '@/core/models/character_model'
 import { PaginationModel } from '@/core/models/pagination_model'
 import { useEffect, useState } from 'react'
 
+export const getCharacters = async({page = '1'}: {page?: string}) => {
+  try {
+    const response: Response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+
+    if(!response.ok){
+      throw new Error('Something went wrong!')
+    }
+
+    const data = await response.json();
+    const pagination = PaginationModel.fromJson(data.info as unknown as{ [key: string]: never; })
+    const characters = data.results?.map((item: Character) => CharacterModel.fromJson(item as unknown as{ [key: string]: never; }))
+
+    return { pagination, characters };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export const useCharacters = () => {
   const [ pagination, setPagination] = useState<Pagination>()
   const [ currentPage, setCurrentPage] = useState<string>('1')
@@ -11,26 +29,16 @@ export const useCharacters = () => {
   const [ isLoading, setIsLoading] = useState<boolean>(true)
   const [ isError, setIsError] = useState<boolean>(false)
 
-  const getCharacters = async({page = '1'}: {page?: string}) => {
-    setIsLoading(true)
-    await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
-      .then( res => res.json()
-      .then((data) => {
-        if(data.error) throw new Error(data.error)
-        
-        setCharacters(data.results?.map((item: Character) => CharacterModel.fromJson(item as unknown as{ [key: string]: never; })) );
-        setPagination(PaginationModel.fromJson(data.info as unknown as{ [key: string]: never; }));
-      })
-      .catch((error) =>{setIsError(error)})
-      .finally(() => setIsLoading(false))
-    );
-
-  };
-
   const handlePage = ({page = '1'}: {page?: string | number}) => {
     const pageStr = page.toString();
 
     getCharacters({page: pageStr})
+      .then(response => {
+        setCharacters(response?.characters);
+        setPagination(response?.pagination);
+      })
+      .catch(error => setIsError(error))
+      .finally(() => setIsLoading(false));
     setCurrentPage(pageStr);
   } 
   
---
+++ components/episodes/index.ts
@@ -0,0 +1,7 @@
+export { CompareList } from './compare_list';
+export { EpisodeEmpty } from './episode_empty';
+export { EpisodeItem } from './episode_item';
+export { EpisodesGrid } from './episodes_grid';
+export { EpisodesList } from './episodes_list';
+export { ListLayout } from './list_layout';
+
